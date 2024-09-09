b0d3ee73153579a0425dd47a090aabdb612d5056
ROBIN PARASHAR <robin.parashar@yahoo.in>
2024-09-09 20:08:16 +0530

    Initial commit

---
+++ Controller/OAuthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TheMarauderMap.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OAuthController : ControllerBase
+    {
+        [HttpGet]
+        [Route("callback")]
+        public async Task<IActionResult> Callback(string code)
+        {
+            // Process the authorization code
+            // Exchange the code for an access token
+            return Ok($"OAuth2 callback successful {code}");
+        }
+
+    }
+}
